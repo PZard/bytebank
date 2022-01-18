bc29c13dafa17b8bbdaf4447b5baf016b9a0b892
Pedro Zardin <pezazagui@gmail.com>
2022-01-18 02:07:51 -0300

    Adição de métodos de Update e Delete no DAO dos contatos

--- lib/database/dao/contact_dao.dart
+++ lib/database/dao/contact_dao.dart
@@ -12,24 +12,11 @@ class ContactDao {
   static const String _name = 'name';
   static const String _accountNumber = 'account_number';
 
-  Future<int> save(Contact contact) async {
-    final Database db = await createDatabase();
-    Map<String, dynamic> contactMap = _toMap(contact);
-    return db.insert(_tableName, contactMap);
-  }
-
   Map<String, dynamic> _toMap(Contact contact) {
     final Map<String, dynamic> contactMap = {};
     contactMap[_name] = contact.name;
     contactMap[_accountNumber] = contact.accountNumber;
     return contactMap;
-  }
-
-  Future<List> findAll() async {
-    final Database db = await createDatabase();
-    final List<Map<String, dynamic>> result = await db.query(_tableName);
-    List<Contact> contacts = _toList(result);
-    return contacts;
   }
 
   List<Contact> _toList(List<Map<String, dynamic>> result) {
@@ -43,5 +30,38 @@ class ContactDao {
       contacts.add(contact);
     }
     return contacts;
+  }
+
+  Future<int> save(Contact contact) async {
+    final Database db = await createDatabase();
+    Map<String, dynamic> contactMap = _toMap(contact);
+    return db.insert(_tableName, contactMap);
+  }
+
+  Future<List> findAll() async {
+    final Database db = await createDatabase();
+    final List<Map<String, dynamic>> result = await db.query(_tableName);
+    List<Contact> contacts = _toList(result);
+    return contacts;
+  }
+
+  Future<int> update(Contact contact) async {
+    final Database db = await createDatabase();
+    final Map<String, dynamic> contactMap = _toMap(contact);
+    return db.update(
+      _tableName,
+      contactMap,
+      where: 'id = ?',
+      whereArgs: [contact.id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    final Database db = await createDatabase();
+    return db.delete(
+      _tableName,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
   }
 }
